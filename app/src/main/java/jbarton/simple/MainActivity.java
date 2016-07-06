47856edf95fd38c2f98490974ef7eb054503e7fd
Craig Bennett <ctbennettii@gmail.com>
2016-07-06 16:24:00 -0400

    Fixed
    There is a ton of comments on what was wrong. Also I made a video on the
    problem
    https://youtu.be/gNJ8pQhqg8E

--- app/src/main/java/jbarton/simple/MainActivity.java
+++ app/src/main/java/jbarton/simple/MainActivity.java
@@ -26,40 +26,71 @@ public class  MainActivity extends AppCompatActivity {
      */
     private GoogleApiClient client;
 
-};
+// }; This was ending the class.  Not sure why there is a ;
 
 
-protected void  onCreate(Bundle savedIntanceState){
+    protected void onCreate(Bundle savedIntanceState) {
         super.onCreate(savedIntanceState);
         setContentView(R.layout.activity_main);
 
-        mnameTextView=(TextView)findViewById(R.id.myname);
-        nschoolTextView=(TextView)findViewById(R.id.school);
-        nButton=(Button)findViewById(R.id.button);
+        mnameTextView = (TextView) findViewById(R.id.myname);
+        nschoolTextView = (TextView) findViewById(R.id.school);
+        nButton = (Button) findViewById(R.id.button);
 
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        View.OnClickListenr listenr=new OnclickListener(){
-public void onClick(view V){
+                String school = "Thank you for clicking the button";
+                String myname = "HelloDearSir";
 
-
-        }
-
-
-        }
-
-
-public void onClick(View v){
-
-        string school="Thank you for clicking the button";
-        string myname="HelloDearSir";
-
-        nschoolTextView.setText(school);
-        mnameTextView.setText(myname);
-        nButton.setVisibility(View.GONE);
+                nschoolTextView.setText(school);
+                mnameTextView.setText(myname);
+                nButton.setVisibility(View.GONE);
+            }
         };
+        nButton.setOnClickListener(listener);
+    }}
 
-        }
-        nButton.setOnClickListener();
+        /**
+         * Below is your old onclick code with the added of where the end of onCreate.
+         * Basically, the first onclick had nothing in it
+         * The strings need to start with a S
+         * nButton.setOnClickListener(); is missing listenr
+         * I made a video to help point out some of this.
+         *      https://youtu.be/gNJ8pQhqg8E
+         *
+         * I have the code working. Please compare it with the comments or the versions.
+         * And, thanks for asking. Please feel free to ask me anything in the future.
+         *
+         *
+
+         View.OnClickListenr listenr = new OnclickListener() {
+         public void onClick(view V) {
+
+
+         }
+
+
+         }
+
+
+         public void onClick(View v) {
+
+         string school = "Thank you for clicking the button";
+         string myname = "HelloDearSir";
+
+         nschoolTextView.setText(school);
+         mnameTextView.setText(myname);
+         nButton.setVisibility(View.GONE);
+         }
+
+         ;
+
+         // } The end of your onCreate was here
+         nButton.setOnClickListener();
+
+         */
 
 
 
